4497d250e0edeca9778107215ceebad0b933f7e0
386dx25 <386dx25@06a07880-4aa5-4118-b3eb-b3f154d13cde>
2013-09-26 15:14:56 +0000

    * glfw callback test

--- trunk/source/demo/demo.cpp
+++ trunk/source/demo/demo.cpp
@@ -1,8 +1,80 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <string>
 
+//static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+//{
+//	if( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
+//		glfwSetWindowShouldClose(window, GL_TRUE);
+//
+//	std::cout << "key " << key << " scancode " << scancode 
+//		 << " action " << action << " mods " << mods << std::endl;
+//}
 
+class Window
+{
+public:
+	Window()
+		: m_window(NULL)
+	{}
 
+	bool init( int width, int height, std::string title )
+	{
+		if( m_window )
+			return false;
+
+		if( !glfwInit() )
+			return -1;
+
+		m_window = glfwCreateWindow( width, height, title.c_str(), NULL, NULL );
+		if( !m_window )
+		{
+			glfwTerminate();
+			return false;
+		}
+
+		glfwMakeContextCurrent( m_window );
+		glfwSetKeyCallback( m_window, Window::key_callback );
+
+		return true;
+	}
+
+	int run()
+	{
+		while( !glfwWindowShouldClose( m_window ) )
+		{
+
+			glfwSwapBuffers( m_window );
+			glfwPollEvents();
+		}
+
+		glfwTerminate();
+
+		return EXIT_SUCCESS;
+	}
+
+protected:
+	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		std::cout << "key " << key << " scancode " << scancode 
+			 << " action " << action << " mods " << mods << std::endl;
+	}
+
+private:
+	GLFWwindow* m_window;
+
+};
+
+#if 1
+int main( int argc, char* argv[] )
+{
+	Window w;
+	if( !w.init(512,512,"Hello GLFW and C++!") )
+		return -1;
+
+	return w.run();
+}
+#else
 int main( int argc, char* argv[] )
 {
 	using namespace std;
@@ -23,6 +95,8 @@ int main( int argc, char* argv[] )
 
 	glfwMakeContextCurrent(window);
 
+	glfwSetKeyCallback(window, key_callback);
+
 	// --- Main loop
 
 	while( !glfwWindowShouldClose( window ) )
@@ -37,3 +111,4 @@ int main( int argc, char* argv[] )
 	glfwTerminate();
 	return EXIT_SUCCESS;
 }
+#endif
