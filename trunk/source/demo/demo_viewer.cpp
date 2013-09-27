0277a059f016147caed41bedfe5fc28d6e0e067f
386dx25 <386dx25@06a07880-4aa5-4118-b3eb-b3f154d13cde>
2013-09-27 14:20:34 +0000

    * Added microphone input and wavedata access to FMODAudio

--- trunk/source/demo/demo_viewer.cpp
+++ trunk/source/demo/demo_viewer.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
+#include <iostream>
 #include <GLFW/glfw3.h>
 #include <e8/viewer/ViewerGLFW.h>
 #include <FMODAudio.h>
@@ -7,6 +8,8 @@
 
 int main( int argc, char* argv[] )
 {
+	using namespace std;
+
 	// --- Setup GLFW window & OpenGL context
 
 	GLFWwindow* window;
@@ -33,19 +36,38 @@ int main( int argc, char* argv[] )
 	// --- Setup FMODAudio
 
 	FMODAudio audio;
-
 	audio.init();
+#if 1
+	// Record from microphone input (no playback)
+	cout << "Available record devices (the first one will be used):" << endl;
+	for( int i=0; i < audio.get_record_driver_info().size(); i++ )
+		cout << i+1 << ": " << audio.get_record_driver_info().at(i) << endl;
+	audio.set_record_driver( 0 );
+	audio.set_record_length( 1./25.f );
+	audio.start_record();
+#else
+	// Playback MP3 music file
 	audio.create_music("G:\\386dx25\\Res\\after work.mp3");
 	audio.play_music();
+#endif
 
 	// --- Main loop
 
+	double time0 = glfwGetTime();
+
 	while( !glfwWindowShouldClose( window ) )
 	{
-		renderer.setSpectrumData( &audio.get_spectrum()[0], audio.get_spectrum().size() );
+		// Delta time
+		double time1 = glfwGetTime();
+		double dt = time1 - time0;
+		time0 = time1;
 
 		// Update demo
-		renderer.update( 0.1f );
+		renderer.setSpectrumData( &audio.get_wavedata()[0],
+		                          (int)audio.get_wavedata().size() );
+		//renderer.setSpectrumData( &audio.get_spectrum()[0], 
+		//                          (int)audio.get_spectrum().size() );
+		renderer.update( (float)dt );
 		renderer.render();
 
 		// Update GLFW
--- trunk/source/fmodplay/fmodplay.cpp
+++ trunk/source/fmodplay/fmodplay.cpp
@@ -12,8 +12,18 @@ int main( int argc, char* argv[] )
 	FMODAudio audio;
 
 	audio.init();
+#if 1
+	// Record from microphone input (no playback)
+	cout << "Available record devices (the first one will be used):" << endl;
+	for( int i=0; i < audio.get_record_driver_info().size(); i++ )
+		cout << i+1 << ": " << audio.get_record_driver_info().at(i) << endl;
+	audio.set_record_driver( 0 );
+	audio.start_record();
+#else
+	// Playback MP3 music file
 	audio.create_music("G:\\386dx25\\Res\\after work.mp3");
 	audio.play_music();
+#endif
 
 	cout << "Hello FMOD!" << endl
 		 << "Press any key to exit..." << endl;
