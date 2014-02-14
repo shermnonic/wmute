f4918aa5a30ce02da1add80e6f06fbf286e5b1f2
386dx25 <386dx25@06a07880-4aa5-4118-b3eb-b3f154d13cde>
2014-02-14 15:29:15 +0000

    * Doxygen main page

---
+++ source/meshspace/doc/Doxymain.h
@@ -0,0 +1,20 @@
+/**
+\mainpage meshspace
+
+\section about About
+
+meshspace is a loose collection of mesh processing code used for a little project by Max Hermann. Besides a utility library \a meshtools it also provides a simple GUI.
+
+This software is part of the project "Explorative Shape Analysis", see the 
+following project page for further details:
+
+http://cg.cs.uni-bonn.de/en/projects/shape-analysis/semantically-steered-visual-analysis-of-shape-spaces/
+
+\section authors Authors
+
+This program is developed by Max Hermann <hermann@cs.uni-bonn.de>.
+
+(c) 2013-2014 by University of Bonn,<br>
+    Institute of Computer Science II,<br>
+	Computer Graphics Group.		
+*/
