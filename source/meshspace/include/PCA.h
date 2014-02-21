c6ece7600b5e6c40dc3566e817db9ae3504773de
386dx25 <386dx25@06a07880-4aa5-4118-b3eb-b3f154d13cde>
2014-02-21 11:48:18 +0000

    * centerMatrix() function in PCA.h

--- source/meshspace/include/PCA.h
+++ source/meshspace/include/PCA.h
@@ -1,8 +1,46 @@
+// Header-only Principal Component Analysis
 // Max Hermann, Feb 2014
 #ifndef PCA_H
 #define PCA_H
 
 #include <Eigen/Dense>
+
+/// Center a data matrix row- or column-wise, i.e. make row/columns zero mean.
+/// @param [in,out] X   Input data matrix will be centered in-place
+/// @param [out]    mu  Column or row average vector
+/// @param [in]     centerRows  Choose if columns or rows are centered
+template <typename Derived1, typename Derived2>
+void centerMatrix( Eigen::MatrixBase<Derived1>& X, Eigen::MatrixBase<Derived2>& mu, 
+	bool centerRows=true )
+{
+	// Center data
+	if( centerRows )
+	{
+		int n = (int)X.cols();
+		
+		mu = X.rowwise().sum() / n;
+		#pragma omp parallel for
+		for( int i=0; i < X.cols(); ++i )
+			X.col(i) -= mu;
+	}
+	else		
+	{
+		int n = (int)X.rows();
+		
+		mu = X.colwise().sum() / n;
+		#pragma omp parallel for
+		for( int i=0; i < X.rows(); ++i )
+			X.row(i) -= mu;
+	}
+}
+
+/// Center a data matrix, convenience wrapper with fewer arguments.
+template <typename Derived1>
+void centerMatrix( Eigen::MatrixBase<Derived1>& X, bool centerRows=true )
+{
+	Eigen::VectorXd mu;
+	centerMatrix( X, mu, centerRows );
+}
 
 /// Principal Component Analysis (PCA) of an arbitrary data matrix
 /// @param [in]  X_ Input dataset with one sample per column
@@ -14,6 +52,7 @@ template <typename Derived1, typename Derived2, typename Derived3, typename Deri
 void computePCA( const Eigen::MatrixBase<Derived1>& X_, Eigen::MatrixBase<Derived2>& PC, 
 	Eigen::MatrixBase<Derived3>& ev, Eigen::MatrixBase<Derived4>& mu )
 {
+	// FIXME: Choose temporary matrix type according to template argument!
 	typedef Eigen::MatrixXd TempMatrix;
 	TempMatrix X( X_ );
 	
@@ -21,10 +60,12 @@ void computePCA( const Eigen::MatrixBase<Derived1>& X_, Eigen::MatrixBase<Derive
 	int n = (int)X.cols();
 	
 	// Center data
-	mu = X.rowwise().sum() / n;
-	#pragma omp parallel for
-	for( int i=0; i < X.cols(); ++i )
-		X.col(i) -= mu;
+	centerMatrix( X, mu );
+		// Same as: 
+		// 		mu = X.rowwise().sum() / n;
+		// 		#pragma omp parallel for
+		// 		for( int i=0; i < X.cols(); ++i )
+		//  		X.col(i) -= mu;
 	
 	// Scatter matrix
 	TempMatrix S;
